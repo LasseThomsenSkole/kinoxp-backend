2456f3820568153913c761346a1ae6f705929d94
hantamil <mira0004@stud.kea.dk>
2024-10-01 11:32:11 +0200

    employee class contructor, getter and setters

--- src/main/java/org/example/kinoxpbackend/model/Employee.java
+++ src/main/java/org/example/kinoxpbackend/model/Employee.java
@@ -22,7 +22,56 @@ public class Employee {
     @Column(nullable = false, length = 50)
     private String password;
 
+    public Employee(){
 
+    }
+
+    public Employee(String name, String email, Role role, String password){
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
 
 enum Role {
