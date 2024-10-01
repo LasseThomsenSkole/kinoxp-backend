bb19a5aafaed0770b82a4212ec1f684115ce8b58
hantamil <mira0004@stud.kea.dk>
2024-10-01 11:29:19 +0200

    showtime class contructor, getter and setters

--- src/main/java/org/example/kinoxpbackend/model/Showtime.java
+++ src/main/java/org/example/kinoxpbackend/model/Showtime.java
@@ -22,6 +22,47 @@ public class Showtime {
     @Temporal(TemporalType.TIMESTAMP)
     private Date showtime; // Date and time
 
+    public Showtime(Movie movie, Theatre theatre, Date showtime){
+        this.movie = movie;
+        this.theatre = theatre;
+        this.showtime = showtime;
+    }
 
+    public Showtime(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Theatre getTheatre() {
+        return theatre;
+    }
+
+    public void setTheatre(Theatre theatre) {
+        this.theatre = theatre;
+    }
+
+    public Date getShowtime() {
+        return showtime;
+    }
+
+
+    public void setShowtime(Date showtime) {
+        this.showtime = showtime;
+    }
 }
 
