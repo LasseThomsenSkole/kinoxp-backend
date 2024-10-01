ccfd9e2013886e7e4b6fd5a3265fee92a216c30c
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-01 10:04:08 +0200

    innit

---
+++ src/main/java/org/example/kinoxpbackend/KinoxpBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.kinoxpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoxpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoxpBackendApplication.class, args);
+    }
+
+}
