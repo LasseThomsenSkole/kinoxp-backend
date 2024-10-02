e6c4bfd0309e8af781c6d8daca0aeda555f27861
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-02 15:07:16 +0200

    UserService for loading users for authentication

--- src/main/java/org/example/kinoxpbackend/service/CustomUserDetailsService.java
+++ src/main/java/org/example/kinoxpbackend/service/CustomUserDetailsService.java
@@ -1,30 +1,36 @@
 package org.example.kinoxpbackend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.kinoxpbackend.model.Role;
 import org.example.kinoxpbackend.repository.UserRepository;
 import org.example.kinoxpbackend.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.example.kinoxpbackend.model.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private UserRepository userRepository;
-
+    private final UserService userService;
     @Override
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return new UserPrincipal(user.getUsername(), user.getPassword(), new ArrayList<>()); //DET HER VIRKER IKKE
+        var user = userService.getUserByUsername(username).orElseThrow();
+        return UserPrincipal.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(List.of(new SimpleGrantedAuthority(user.getRole().name())))
+                .build();
     }
 }
---
+++ src/main/java/org/example/kinoxpbackend/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.kinoxpbackend.service;
+
+import org.example.kinoxpbackend.model.Role;
+import org.springframework.stereotype.Service;
+import org.example.kinoxpbackend.model.User;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private static final String EXISTING_USERNAME = "UserTest";
+
+    public Optional<User> getUserByUsername(String username) {
+        if (! EXISTING_USERNAME.equals(username)) return Optional.empty();
+
+        User user = new User();
+        user.setId(1);
+        user.setUsername(EXISTING_USERNAME);
+        user.setPassword("$2a$12$G20tn3gThoAyHKBp6nJyh.SCUivWS7MN5qtc6AU687vkfiOSiSA9C"); // password: "test"
+        user.setRole(Role.USER);
+        return Optional.of(user);
+    }
+
+}
