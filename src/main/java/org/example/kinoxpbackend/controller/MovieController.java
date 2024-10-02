35b59600d64542738f32173e0a5189ce564e0ad4
hantamil <mira0004@stud.kea.dk>
2024-10-02 12:02:49 +0200

    mappings for moviecontroller

--- src/main/java/org/example/kinoxpbackend/controller/MovieController.java
+++ src/main/java/org/example/kinoxpbackend/controller/MovieController.java
@@ -1,4 +1,40 @@
 package org.example.kinoxpbackend.controller;
 
+import org.example.kinoxpbackend.model.Movie;
+import org.example.kinoxpbackend.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @PostMapping("/create-movie")
+    public String createMovie(@RequestBody Movie movie){
+        movieService.createMovie(movie);
+        return "/";
+    }
+
+    @PostMapping("/delete-movie")
+    public void deleteMovie(@PathVariable int id){
+        movieService.deleteMovie(id);
+    }
+
+    @PostMapping("/edit-movie")
+    public String editMovie(@PathVariable int id, @RequestBody Movie updatedMovie){
+        movieService.editMovie(id, updatedMovie);
+        return "/";
+    }
+
+    @GetMapping("/all-movies")
+    public List<Movie> getAllMovies(){
+        movieService.findAllMovies();
+        return "/";
+    }
 }
