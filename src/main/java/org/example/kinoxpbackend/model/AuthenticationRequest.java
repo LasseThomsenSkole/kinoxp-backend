18e8f185210f6e4247f63443348b3e50f220446f
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-02 11:53:17 +0200

    lombok getter & setter

--- src/main/java/org/example/kinoxpbackend/model/AuthenticationRequest.java
+++ src/main/java/org/example/kinoxpbackend/model/AuthenticationRequest.java
@@ -1,10 +1,12 @@
 package org.example.kinoxpbackend.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Builder
 public class AuthenticationRequest {
     private String username;
     private String password;
