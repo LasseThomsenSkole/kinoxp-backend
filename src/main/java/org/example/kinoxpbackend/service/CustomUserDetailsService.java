6c4486f4045c9ab3f27e7b4a0eadb099c2efd664
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-03 15:32:40 +0200

    throw username not found exception

--- src/main/java/org/example/kinoxpbackend/service/CustomUserDetailsService.java
+++ src/main/java/org/example/kinoxpbackend/service/CustomUserDetailsService.java
@@ -1,21 +1,16 @@
 package org.example.kinoxpbackend.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.kinoxpbackend.model.Role;
-import org.example.kinoxpbackend.repository.UserRepository;
-import org.example.kinoxpbackend.security.UserPrincipal;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.example.kinoxpbackend.model.User;
+import org.example.kinoxpbackend.security.UserPrincipal;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Component
@@ -23,14 +18,17 @@ import java.util.List;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserService userService;
+
     @Override
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = userService.getUserByUsername(username).orElseThrow();
+        User user = userService.getUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
         return UserPrincipal.builder()
                 .id(user.getId())
                 .username(user.getUsername())
                 .password(user.getPassword())
-                .authorities(List.of(new SimpleGrantedAuthority(user.getRole().name())))
+                .authorities(List.of(new SimpleGrantedAuthority(user.getRole().name()))) // Use the enum's name
                 .build();
     }
 }
