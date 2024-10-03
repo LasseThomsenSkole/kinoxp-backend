724434f5a7048dfd94d9d172b6b9cb526ed70d41
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-03 15:34:21 +0200

    jwt filter to set UserPrincipal

---
+++ src/main/java/org/example/kinoxpbackend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package org.example.kinoxpbackend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        getJwtFromRequest(request)
+                .map(jwtUtil::extractAllClaims) // Extract claims from JWT
+                .map(jwtUtil::jwtToUserPrincipal) // Convert claims to UserPrincipal
+                .map(UserPrincipalAuthenticationToken::new) // Use lambda instead of method reference
+                .ifPresent(authentication -> {
+                    // Set authentication in SecurityContext
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                });
+
+        filterChain.doFilter(request, response);
+    }
+
+
+    private Optional<String> getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return Optional.of(bearerToken.substring(7));
+        }
+        return Optional.empty();
+    }
+}
