a1e61d04555627b70ad2af5f35e379f23c09cc72
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-03 15:35:46 +0200

    lombok NoArgsConstructor,getter & setter for simplicity

--- src/main/java/org/example/kinoxpbackend/model/User.java
+++ src/main/java/org/example/kinoxpbackend/model/User.java
@@ -5,20 +5,17 @@ import lombok.*;
 
 @Entity
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    public User() {
-    }
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-
     @Column(nullable = false, length = 50)
     private String username;
-
 
     @Column(nullable = false, length = 60)
     private String password;
@@ -26,36 +23,4 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
