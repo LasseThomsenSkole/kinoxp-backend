c71f46d894c0e4435c3c1e871d495b3113eff31e
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-03 15:43:57 +0200

    GetUserByUsername now uses the userRepository

--- src/main/java/org/example/kinoxpbackend/service/UserService.java
+++ src/main/java/org/example/kinoxpbackend/service/UserService.java
@@ -1,15 +1,20 @@
 package org.example.kinoxpbackend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.example.kinoxpbackend.model.Role;
+import org.example.kinoxpbackend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.example.kinoxpbackend.model.User;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
     private static final String EXISTING_USERNAME = "UserTest";
-    //TODO skal laves om til database
+    /*TODO skal laves om til database
     public Optional<User> getUserByUsername(String username) {
         if (! EXISTING_USERNAME.equals(username)) return Optional.empty();
 
@@ -19,6 +24,8 @@ public class UserService {
         user.setPassword("$2a$12$G20tn3gThoAyHKBp6nJyh.SCUivWS7MN5qtc6AU687vkfiOSiSA9C"); // password: "test"
         user.setRole(Role.USER);
         return Optional.of(user);
+    }*/
+    public User getUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
     }
-
 }
