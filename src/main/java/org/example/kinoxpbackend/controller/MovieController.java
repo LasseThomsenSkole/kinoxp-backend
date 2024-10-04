d0dbda16e46d3c5f6ff42de322f9542db517f994
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-10-04 11:26:46 +0200

    added movie path as /movie/

--- src/main/java/org/example/kinoxpbackend/controller/MovieController.java
+++ src/main/java/org/example/kinoxpbackend/controller/MovieController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/movie")
 public class MovieController {
 
     @Autowired
