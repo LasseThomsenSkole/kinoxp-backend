f22a9a60b26734378f84aa3512c6ca4798824ce1
AndreaRyholt <anra0003@stud.kea.dk>
2024-10-09 17:44:33 +0200

    Kan ikke huske

--- src/main/java/org/example/kinoxpbackend/service/PriceCalculatorService.java
+++ src/main/java/org/example/kinoxpbackend/service/PriceCalculatorService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class PriceCalculatorService {
 
-
     private Movie movieClass = new Movie();
     private Seat seatClass = new Seat();
     private Reservation reservationClass = new Reservation();
